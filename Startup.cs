83ba9d4ea88ddebaed8008f5532531bee73fcfa7
phuocleoceo <ht10082001@gmail.com>
2021-06-15 00:34:06 +0700

    Some option for Identity

--- Startup.cs
+++ Startup.cs
@@ -36,7 +36,32 @@ namespace IdentityRazor
 			services.AddIdentity<AppUser, IdentityRole>()
 					.AddEntityFrameworkStores<IRContext>()
 					.AddDefaultTokenProviders();
+			// Change Default setting of Identity
+			services.Configure<IdentityOptions>(options =>
+			{
+				// Thiết lập về Password
+				options.Password.RequireDigit = false; // Không bắt phải có số
+				options.Password.RequireLowercase = false; // Không bắt phải có chữ thường
+				options.Password.RequireNonAlphanumeric = false; // Không bắt ký tự đặc biệt
+				options.Password.RequireUppercase = false; // Không bắt buộc chữ in
+				options.Password.RequiredLength = 3; // Số ký tự tối thiểu của password
+				options.Password.RequiredUniqueChars = 1; // Số ký tự riêng biệt
 
+				// Cấu hình Lockout - khóa user
+				options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5); // Khóa 5 phút
+				options.Lockout.MaxFailedAccessAttempts = 5; // Thất bại 5 lầ thì khóa
+				options.Lockout.AllowedForNewUsers = true;
+
+				// Cấu hình về User.
+				options.User.AllowedUserNameCharacters = // các ký tự đặt tên user
+					"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+				options.User.RequireUniqueEmail = true;  // Email là duy nhất
+
+				// Cấu hình đăng nhập.
+				options.SignIn.RequireConfirmedEmail = true;            // Cấu hình xác thực địa chỉ email (email phải tồn tại)
+				options.SignIn.RequireConfirmedPhoneNumber = false;     // Xác thực số điện thoại
+			});
+			//
 			services.AddRazorPages();
 		}
 
