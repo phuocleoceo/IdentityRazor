46a9bacee78b359022b4ccc13a4733edaae5ad23
phuocleoceo <ht10082001@gmail.com>
2021-06-15 21:17:57 +0700

    Register&Email Confirm

--- Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -12,36 +12,46 @@ using Microsoft.AspNetCore.WebUtilities;
 
 namespace IdentityRazor.Areas.Identity.Pages.Account
 {
-    [AllowAnonymous]
-    public class ConfirmEmailModel : PageModel
-    {
-        private readonly UserManager<AppUser> _userManager;
+	[AllowAnonymous]
+	public class ConfirmEmailModel : PageModel
+	{
+		private readonly UserManager<AppUser> _userManager;
+		private readonly SignInManager<AppUser> _signInManager;
 
-        public ConfirmEmailModel(UserManager<AppUser> userManager)
-        {
-            _userManager = userManager;
-        }
+		public ConfirmEmailModel(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+		{
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
 
-        [TempData]
-        public string StatusMessage { get; set; }
+		[TempData]
+		public string StatusMessage { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(string userId, string code)
-        {
-            if (userId == null || code == null)
-            {
-                return RedirectToPage("/Index");
-            }
+		public async Task<IActionResult> OnGetAsync(string userId, string code)
+		{
+			if (userId == null || code == null)
+			{
+				return RedirectToPage("/Index");
+			}
 
-            var user = await _userManager.FindByIdAsync(userId);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{userId}'.");
-            }
+			var user = await _userManager.FindByIdAsync(userId);
+			if (user == null)
+			{
+				return NotFound($"Unable to load user with ID '{userId}'.");
+			}
 
-            code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
-            var result = await _userManager.ConfirmEmailAsync(user, code);
-            StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
-            return Page();
-        }
-    }
+			code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
+			var result = await _userManager.ConfirmEmailAsync(user, code);
+			if (result.Succeeded)
+			{
+				StatusMessage = "Thank you for confirming your email.";
+				await _signInManager.SignInAsync(user, false);
+			}
+			else
+			{
+				StatusMessage = "Error confirming your email.";
+			}
+			return Page();
+		}
+	}
 }
