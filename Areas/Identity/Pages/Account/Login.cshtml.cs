05e6a6410836910c0c2c17d92fff610307dcfa23
phuocleoceo <ht10082001@gmail.com>
2021-06-16 14:01:40 +0700

    Fix Login Classic

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -44,8 +44,8 @@ namespace IdentityRazor.Areas.Identity.Pages.Account
 		public class InputModel
 		{
 			[Required]
-			[EmailAddress]
-			public string Email { get; set; }
+			[StringLength(100, MinimumLength = 1)]
+			public string UserNameOrEmail { set; get; }
 
 			[Required]
 			[DataType(DataType.Password)]
@@ -86,15 +86,22 @@ namespace IdentityRazor.Areas.Identity.Pages.Account
 
 			if (ModelState.IsValid)
 			{
-				AppUser user = await _userManager.FindByEmailAsync(Input.Email);
-				if (user == null)
-				{
-					ModelState.AddModelError(string.Empty, "User is not exists.");
-					return Page();
-				}
 				// This doesn't count login failures towards account lockout
 				// To enable password failures to trigger account lockout, set lockoutOnFailure: true
-				var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: true);
+				var result = await _signInManager.PasswordSignInAsync(Input.UserNameOrEmail, Input.Password, Input.RememberMe, lockoutOnFailure: true);
+				if (!result.Succeeded)
+				{
+					var user = await _userManager.FindByEmailAsync(Input.UserNameOrEmail);
+					if (user != null)
+					{
+						result = await _signInManager.PasswordSignInAsync(
+							user,
+							Input.Password,
+							Input.RememberMe,
+							true
+						);
+					}
+				}
 				if (result.Succeeded)
 				{
 					_logger.LogInformation("User logged in.");
